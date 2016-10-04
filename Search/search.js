2e5d5288a05f22870bb9e9591c782444bd5e25ed
RobinsonKameron <ka12@umbc.edu>
2016-10-04 13:40:19 +0200

    Update search class

--- Search/search.js
+++ Search/search.js
@@ -1,41 +1,39 @@
 'use strict'
-
-/**
-    This module is using JSON query to search my obsucre places json
-    https://github.com/mmckegg/json-query
-  */
-
 const jsonQuery = require('json-query')
 
-class Search {
-  /**
-   * getPlaces allws you to search any json you provide it with regex
-   *
-   * @param {object} object - Contains 4 keys
-   *  var example = {
-   *   json: the json object,
-   *   selectAll: used to select data from a database,
-   *   where: used to extract only those records that fulfill a specified criterion
-   *  }
-   *
-   * use ~ Search.getPlaces(example)
-   * @return {object} - contains the result of your query => {value: 'x', parents: [...], key: 0} ... etc
-   *
-   */
-  static getPlaces(object) {
-      let q = jsonQuery(`[*${object.selectAll}~/${object.where}/i]:`, {
-        data: object.json,
-        allowRegexp: true
-      })
-      return q
-    } // getPlaces
+/**
+ *  This module is using JSON query to search my obsucre places json
+ *  https://github.com/mmckegg/json-query
+ *  getPlacesByLocation allws you to search any json you provide it with regex
+ *
+ * @param {object} object - Contains 4 keys
+ *  var example = {
+ *   json: the json object,
+ *   selectAll: used to select data from a database,
+ *   where: used to extract only those records that fulfill a specified criterion
+ *  }
+ *
+ * use ~ Search.getPlaces(example)
+ * @return {object} - contains the result of your query => {value: 'x', parents: [...], key: 0} ... etc
+ *
+ */
+
+const search = {
+  getPlacesByLocation: (object) => {
+    let q = jsonQuery(`[*${object.selectAll}~/${object.where}/i]:`, {
+      data: object.json,
+      allowRegexp: true
+    })
+    return q
+  } // getPlaces
 }
 
-const obscurePlaces = require('../data/places/atlasObscurePlaces.json')
-console.log(Search.getPlaces({
-  json: obscurePlaces,
-  selectAll: 'name',
-  where: 'New York'
-}).value)
 
-exports.module = Search
+// const obscurePlaces = require('../data/places/atlasObscurePlaces.json')
+// console.log(Search.getPlaces({
+//   json: obscurePlaces,
+//   selectAll: 'name',
+//   where: 'New York'
+// }).value)
+
+module.exports = search
