6f5aa69a5d005fe72b5d7e4ea12dcc96c5cad92b
RobinsonKameron <ka12@umbc.edu>
2016-10-04 13:41:14 +0200

    Small changes to routes, data, package.json

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
-module.exports = function(express, app, passport, config, rooms) {
-  var router = express.Router()
+module.exports = function(express, app, passport, config) {
+  const router = express.Router()
+  const Search = require('../Search/search.js')
 
-  app.get('/', function(req, res) {
-
+  app.get('/', (req, res) => {
     if (req.user) {
       res.render('home', {
         user: req.user,
@@ -15,7 +15,7 @@ module.exports = function(express, app, passport, config, rooms) {
     }
   })
 
-  function securePages(req, res, next) {
+  var securePages = (req, res, next) => {
     if (req.isAuthenticated()) {
       next()
     } else {
@@ -29,17 +29,50 @@ module.exports = function(express, app, passport, config, rooms) {
     failureRedirect: '/'
   }))
 
-  router.get('/home', securePages, function(req, res, next) {
+  router.get('/home', securePages, (req, res, next) => {
     res.render('index', {
       user: req.user,
       config: config
     })
   })
 
-  router.get('/logout', securePages, function(req, res, next) {
+  router.post('/newTrip',(req,res,next) => {
+    if (req.method.toLowerCase() === 'post') {
+        const location = req.body.location
+        const route = `/#/newTrip`
+        res.send({redirect: route, location: location})
+    }
+  })
+
+  router.get('/logout', securePages, (req, res, next) => {
     // console.log(req.user)
     req.logout()
     res.redirect('/')
+  })
+
+  // Search interesting places API
+
+  /**
+  example use in front end with jQuery
+  $.getJSON("/searchPlace", {
+      loc: "New York",
+      format: "jsonp"
+    },
+    function(data) {
+      console.log(data)
+    });
+   */
+
+  router.get('/searchPlace', (req, res, next) => {
+    const obscurePlaces = require('../public/places/atlasObscurePlaces.json')
+    const response = Search.getPlacesByLocation({
+      json: obscurePlaces,
+      selectAll: 'name',
+      where: req.query.loc
+    }).value
+
+    res.json(response)
+
   })
 
   app.use('/', router)
