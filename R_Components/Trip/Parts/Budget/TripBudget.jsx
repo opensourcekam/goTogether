34ef81b6659e5485459fce4b9a96b3c67ba922b3
RobinsonKameron <ka12@umbc.edu>
2016-10-18 18:04:13 +0200

    Updates

--- R_Components/Trip/Parts/Budget/TripBudget.jsx
+++ R_Components/Trip/Parts/Budget/TripBudget.jsx
@@ -4,7 +4,7 @@ class TripBudget extends React.Component {
   render () {
     return (
       <div className="budget">
-        <canvas>STUFF FOR BUDGET</canvas>
+        <canvas style={{visibility: 'hidden'}}>STUFF FOR BUDGET</canvas>
       </div>
     )
   }
--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const ConnectMongo = require('connect-mongo')(session)
 const mongoose = require('mongoose').connect(config.dburl)
 const passport = require('passport')
 const FacebookStrategy = require('passport-facebook').Strategy
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const port = 8080
 const env = process.env.NODE_ENV || 'development'
+
+require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose)
+require('./routes/routes.js')(express, app, passport, config)
+require('./socket/socket.js')(io)
 
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'views'))
@@ -18,7 +24,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/public')))
 app.use(require('body-parser').urlencoded({ extended: true }))
 app.use(require('morgan')('combined'))
-
 
 if (env === 'development') {
   // dev specific settings
@@ -41,14 +46,6 @@ if (env === 'development') {
 
 app.use(passport.initialize())
 app.use(passport.session())
-
-
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-
-require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose)
-require('./routes/routes.js')(express, app, passport, config)
-require('./socket/socket.js')(io)
 
 app.set('port', process.env.PORT || port)
 server.listen(app.get('port'), function () {
