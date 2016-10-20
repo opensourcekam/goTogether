4cf4e7b70d020b22c2835d2d2142876571805bd1
RobinsonKameron <ka12@umbc.edu>
2016-10-20 21:14:36 +0200

    Shifted some code added a few comments

--- app.js
+++ app.js
@@ -11,56 +11,48 @@ const FacebookStrategy = require('passport-facebook').Strategy
 const port = 8080
 const env = process.env.NODE_ENV || 'development'
 
+// Middleware
+const forceSsl = function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''))
+  }
+  return next()
+}
+
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'views'))
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'))
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/public')))
-app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(require('body-parser').urlencoded({extended: true}))
 app.use(require('morgan')('combined'))
-
-const forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''))
-    }
-    return next()
- }
 
 if (env === 'development') {
   // dev specific settings
-  app.use(session({
-    secret: config.sessionSecret,
-    saveUninitialized: true,
-    resave: true
-  }))
+  app.use(session({secret: config.sessionSecret, saveUninitialized: true, resave: true}))
 } else {
   // production specific settings
   app.use(forceSsl)
   app.use(session({
     secret: config.sessionSecret,
-    store: new ConnectMongo({
-      url: config.dburl,
-      mongooseConnection: mongoose.connection,
-      stringify: true
-    })
+    store: new ConnectMongo({url: config.dburl, mongooseConnection: mongoose.connection, stringify: true})
   }))
-}// endelse
+} // endelse
 
 app.use(passport.initialize())
 app.use(passport.session())
-
+app.set('port', process.env.PORT || port)
+// End Middleware
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-
 require('./auth/passportAuth.js')(passport, FacebookStrategy, config, mongoose)
 require('./routes/routes.js')(express, app, passport, config)
 require('./socket/socket.js')(io)
 
-app.set('port', process.env.PORT || port)
-server.listen(app.get('port'), function () {
+server.listen(app.get('port'), function() {
   console.log(`
     Listening on http://localhost:${port}
     Environment: ${env}
