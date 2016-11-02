a5c9f16236537bf71576d9abee6812dee1098e9a
RobinsonKameron <ka12@umbc.edu>
2016-11-02 12:19:50 +0100

    Added MyTrips folder with new components for rendering user trips

---
+++ R_Components/MyTrips/MyTrips.jsx
@@ -0,0 +1,31 @@
+const React = require('react')
+
+class MyTrips extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  // Get each trip for currently loggedin user
+  // pass props to DisplayTrip component
+  // Dispaly each trip make clickable
+  // Each trip will feed its props into a the PlanNewTrip Dashboard component
+
+  componentWillMount () {
+
+  }
+
+  componentDidMount () {
+
+  }
+
+  componentWillUnmount () {
+  }
+
+  render () {
+    return (
+      <div>MyTrips</div>
+    )
+  }
+}
+
+module.exports = MyTrips
