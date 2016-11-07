088cb6a0232547335a8450399cc5f4e1119c530d
Jared Johnson <jjohnson.eit@gmail.com>
2016-11-06 20:14:53 -0500

    add login, logout, and signup to user actions and user middleware

--- redux/actions/UserActions.js
+++ redux/actions/UserActions.js
@@ -3,6 +3,23 @@
 export const RECEIVE_USER = 'RECEIVE_USER'
 export const REQUEST_USER = 'REQUEST_USER'
 export const UPDATE_USER = 'UPDATE_USER'
+export const LOGIN = 'LOGIN'
+export const LOGOUT = 'LOGOUT'
+export const SIGNUP = 'SIGNUP'
+
+export const login = user => ({
+  type: LOGIN,
+  user
+})
+
+export const logout = () => ({
+  type: LOGOUT
+})
+
+export const signup = user => ({
+  type: SIGNUP,
+  user
+})
 
 export const receiveUser = user => ({
   type: RECEIVE_USER,
--- redux/middleware/UserMiddleware.js
+++ redux/middleware/UserMiddleware.js
@@ -1,14 +1,29 @@
 import {
   REQUEST_USER,
-  UPDATE_USER
-  // receiveUser
+  UPDATE_USER,
+  LOGIN,
+  LOGOUT,
+  SIGNUP,
+  receiveUser,
+  login,
+  logout,
+  signup
 } from '../actions/UserActions'
 
 export default ({ getState, dispatch }) => next => action => {
   // callbacks
-  // const success = user => dispatch(receiveUser(user))
-  // const error = messages => console.log(messages)
+  const success = user => dispatch(receiveUser(user))
+  const error = response => console.log(response)
   switch (action.type) {
+    case LOGIN:
+      login(action.user, success, error)
+      return next(action)
+    case LOGOUT:
+      logout(() => next(action))
+      break
+    case SIGNUP:
+      signup(action.user, success, error)
+      break
     case REQUEST_USER:
       // make AJAX GET request here using success/error callbacks
       return next(action)
