19d373dfaabdde52b163e14bf29e8c2641790dc3
RobinsonKameron <ka12@umbc.edu>
2016-11-08 02:17:39 +0100

    Abstracted most global middleware from app.js to middleware.js (may rename to global_middleware tomorrow)

--- app.js
+++ app.js
@@ -10,23 +10,7 @@ const passport = require('passport')
 const FacebookStrategy = require('passport-facebook').Strategy
 const port = 8080
 const env = process.env.NODE_ENV || 'development'
-
-// DB
-// Mongoose models (should abstract out soon)
-const user = require('./Database/userSchema')(mongoose)
-const UserModel = mongoose.model('user', user)
-
-const TripModel = require('./Database/tripsSchema')(mongoose)
-const Trip = mongoose.model('Trips', TripModel)
-// DB
-
-// Middleware
-const forceSsl = function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''))
-  }
-  return next()
-}
+const axios = require('axios')
 
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'views'))
@@ -35,39 +19,31 @@ app.set('views', path.join(__dirname, 'views'))
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/public')))
-app.use(require('body-parser').urlencoded({extended: true}))
-app.use(require('body-parser').json())
-app.use(require('morgan')('combined'))
-app.set('json spaces', 3)
 
-if (env === 'development') {
-  // dev specific settings
-  app.use(session({secret: config.sessionSecret, saveUninitialized: true, resave: true}))
-} else {
-  // production specific settings
-  app.use(forceSsl)
-  app.use(session({
-    secret: config.sessionSecret,
-    store: new ConnectMongo({url: config.dburl, mongooseConnection: mongoose.connection, stringify: true})
-  }))
-} // endelse
+// DB
+// Mongoose models (should abstract out soon)
+const UserModel = require('./Database/userSchema')(mongoose)
+const User = mongoose.model('User', UserModel)
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.set('port', process.env.PORT || port)
+const TripModel = require('./Database/tripsSchema')(mongoose)
+const Trip = mongoose.model('Trips', TripModel)
+// DB
+
+// Middleware
+const middleware = require('./Middleware/middleware')(express, cookieParser, app, mongoose, ConnectMongo, passport, port, env, session, config)
 // End Middleware
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
 // routes
-require('./routes/routes')(express, app, passport, config, user, UserModel)
-require('./routes/trips')(express, app, mongoose, TripModel, Trip, UserModel)
+require('./routes/routes')(express, app, passport, config, User, UserModel, axios)
+require('./routes/trips')(express, app, mongoose, TripModel, Trip, User)
 require('./routes/SkyscannerAPIV1')(express, app, config)
-
+// routes
 
 // passport auth (FB)
-require('./auth/passportAuth')(passport, FacebookStrategy, config, mongoose, user, UserModel)
+require('./auth/passportAuth')(passport, FacebookStrategy, config, mongoose, User, UserModel, Trip)
 
 // socket.io
 require('./socket/socket')(io)
