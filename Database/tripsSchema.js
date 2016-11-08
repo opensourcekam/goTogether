20cbad7f68cf764a35765cc11da527a5f23b4fba
RobinsonKameron <ka12@umbc.edu>
2016-11-08 02:14:55 +0100

    Updates to DB schemas

--- Database/tripsSchema.js
+++ Database/tripsSchema.js
@@ -2,8 +2,8 @@ module.exports = function(mongoose) {
   const Schema = mongoose.Schema
   return new Schema({
     _creator: {
-      type: Schema.Types.ObjectId,
-      ref: 'user'
+      type: Number,
+      ref: 'User'
     },
     to: {
       type: String,
@@ -12,27 +12,27 @@ module.exports = function(mongoose) {
     from: {
       type: String
     },
-    created: {
-      type: Date,
-      default: Date.now
-    },
-    updated: {
-      type: Date,
-      default: Date.now
-    },
     tripDate: {
       type: Date,
       date: Date
-    },
-    past: {
-      type: Boolean,
-      default: false
     },
     budget: {
       type: Number,
       default: 100
     },
     meta: {
+      created: {
+        type: Date,
+        default: Date.now
+      },
+      updated: {
+        type: Date,
+        default: Date.now
+      },
+      past: {
+        type: Boolean,
+        default: false
+      },
       votes: {
         type: Number,
         default: 0
@@ -45,42 +45,35 @@ module.exports = function(mongoose) {
       percantagePlanned: {
         type: Number,
         default: 0
-      }
-    },
-    invitees: {
-      type: Array
+      },
+      planeBooked: {
+        type: Boolean,
+        default: false
+      },
+      trainBooked: {
+        type: Boolean,
+        default: false
+      },
+      busBooked: {
+        type: Boolean,
+        default: false
+      },
+      hotelBooked: {
+        type: Boolean,
+        default: false
+      },
+      hostelBooked: {
+        type: Boolean,
+        default: false
+      },
+      stayingWithFamilyOrFriend: {
+        type: Boolean,
+        default: false
+      },
+      invitees: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+      }]
     }
   }, {collection: 'Trips'})
-
-  // const Trip = mongoose.model('trip', TripModel)
-  //
-  // // remove all trips
-  // Trip.remove({}, function(err) {
-  //   console.log('collection removed')
-  // })
-  //
-  // // test creating new trip
-  // Trip.create({
-  //   '_creator': '5817256e3309cb0d41f25310',
-  //   'to': 'Cassis, France',
-  //   'tripDate': new Date(Date.parse("January 21, 2017")).toString(),
-  //   'budget': '1500',
-  //   'past': false,
-  //   'meta': {
-  //     'favs': 10
-  //   }
-  // }, (err, doc) => {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-  //     console.log(`New trip created ${doc}`)
-  //   }
-  // })
-  //
-  // // Populating is similar to joining a table in SQL it will pop on the model at type: Schema.Types.ObjectId,
-  // const promise = Trip.find({}).populate('_creator').exec()
-  // promise.then((trips) => {
-  //   console.log(`All trips include ${trips}`)
-  // })
-
 }
--- Database/userSchema.js
+++ Database/userSchema.js
@@ -1,7 +1,9 @@
 module.exports = function(mongoose) {
+    const Schema = mongoose.Schema
     return new mongoose.Schema({
-    profileID: String,
-    fullName: String,
-    profilePic: String
+      profileID: String,
+      fullName: String,
+      profilePic: String,
+      trips : [{ type: Schema.Types.ObjectId, ref: 'Trips' }]
   })
 }
