53ee50c2e769503d02a75a0e1fc43f6abf77e43f
robinsonKameron <ka12@umbc.edu>
2017-03-23 02:58:28 -0400

    UPDATE DisplayPhoto css

--- frontend/R_Components/Trip/Parts/DisplayPhoto.jsx
+++ frontend/R_Components/Trip/Parts/DisplayPhoto.jsx
@@ -1,20 +1,43 @@
 /* global userJSON */
 /* eslint no-undef: "error" */
-const React = require('react')
+const React = require('react');
+const style = {
+  'figure': {
+    'display': 'inline-flex',
+    'width': '120px',
+    'height': '120px',
+    'position': 'relative',
+    'overflow': 'hidden',
+    'WebkitBorderRadius': '50%',
+    'MozBorderRadius': '50%',
+    'MsBorderRadius': '50%',
+    'OBorderRadius': '50%',
+    'borderRadius': '50%',
+    'marginTop': '10px'
+  },
+  'img': {
+    'display': 'inline',
+    'margin': '0 auto',
+    'height': '100%',
+    'width': '100%'
+  }
+};
 
 class DisplayPhoto extends React.Component {
   constructor (props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   render () {
     return (
-      <figure>
-        <img src={(userJSON.photos) ? userJSON.photos[0].value : ''} />
-      </figure>
-    )
+      <div style={style.figure}>
+        <img src={(userJSON.photos)
+          ? userJSON.photos[0].value
+          : ''} style={style.img} />
+      </div>
+    );
   }
 }
 
-module.exports = DisplayPhoto
+module.exports = DisplayPhoto;
